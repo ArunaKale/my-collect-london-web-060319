152997b7d30e35e55bcda5a16bef7c69359c563a
AN K <a2nkale@gmail.com>
2019-05-31 23:25:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  i= 0 
+  while i < array.length 
+  yield (array[i])
+  i += 1
+ end 
+ array
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |student|
+  student.split(" ").first
+  array
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+  collection
+end
+ 
